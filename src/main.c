b763be3cfe94b798d71def4691cd270dbff835ea
T0n0T <823478402@qq.com>
2024-02-29 00:05:14 +0800

    update

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 #include "demos/lv_demos.h"
 #include "examples/lv_examples.h"
 #include <stdio.h>
-#include <widgets/lv_demo_widgets.h>
 
 int main(int argc, char const *argv[])
 {
     lv_porting_init();
     printf("LVGL porting init\n");
     lv_example_ffmpeg_1();
+    // lv_example_arc_1();
     while (1) {
         lv_timer_handler();
         lv_porting_delay();
